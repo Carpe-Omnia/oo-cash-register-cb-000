ba72d55b247847977836ed2945aa78e0cbbabdae
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2018-12-13 21:24:34 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,19 @@
-
+class CashRegister
+  attr_accessor :total, :discount, :items
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+  def add_item(title, price, quanitity=1)
+    @total += price * quantity
+    @items << title
+  end
+  def apply_discount
+    @total = @total * ((100-@discount)/100)
+    return "After the discount, the total comes to $#{total}."
+  end
+  def items 
+    return @items
+  end   
+end
